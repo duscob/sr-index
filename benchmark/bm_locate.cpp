1686311ea36f2b9b150cd467ffd12dab48bb0c84
Dustin Cobas <dustin.cobas@gmail.com>
2021-01-17 00:48:40 -0300

    Add lean variant to benchmark

--- benchmark/bm_locate.cpp
+++ benchmark/bm_locate.cpp
@@ -15,16 +15,20 @@ DEFINE_string(data_dir, "./", "Data directory.");
 DEFINE_string(data_name, "data", "Data file basename.");
 DEFINE_bool(print_result, false, "Execute benchmark that print results per index.");
 
+void SetupDefaultCounters(benchmark::State &t_state) {
+  t_state.counters["Size(bytes)"] = 0;
+  t_state.counters["Bits_x_Symbol"] = 0;
+  t_state.counters["Patterns"] = 0;
+  t_state.counters["Time_x_Pattern"] = 0;
+}
+
 // Benchmark Warm-up
 static void BM_WarmUp(benchmark::State &_state) {
   for (auto _ : _state) {
     std::string empty_string;
   }
 
-  _state.counters["Size(bytes)"] = 0;
-  _state.counters["Bits_x_Symbol"] = 0;
-  _state.counters["Patterns"] = 0;
-  _state.counters["Time_x_Pattern"] = 0;
+  SetupDefaultCounters(_state);
 }
 BENCHMARK(BM_WarmUp);
 
@@ -37,6 +41,7 @@ auto BM_QueryLocate =
         }
       }
 
+      SetupDefaultCounters(_state);
       _state.counters["Size(bytes)"] = _idx.second;
       _state.counters["Bits_x_Symbol"] = _idx.second * 8.0 / _seq_size;
       _state.counters["Patterns"] = _patterns.size();
@@ -62,6 +67,7 @@ auto BM_PrintQueryLocate =
         }
       }
 
+      SetupDefaultCounters(_state);
       _state.counters["Size(bytes)"] = _idx.second;
       _state.counters["Bits_x_Symbol"] = _idx.second * 8.0 / _seq_size;
       _state.counters["Patterns"] = _patterns.size();
@@ -109,6 +115,11 @@ int main(int argc, char *argv[]) {
       {"R-Index-WS/16", Factory::Config{Factory::IndexEnum::RIndexSampled, 16}},
       {"R-Index-WS/32", Factory::Config{Factory::IndexEnum::RIndexSampled, 32}},
       {"R-Index-WS/64", Factory::Config{Factory::IndexEnum::RIndexSampled, 64}},
+      {"R-Index-WS-TM/4", Factory::Config{Factory::IndexEnum::RIndexSampledWithTrustedMarks, 4}},
+      {"R-Index-WS-TM/8", Factory::Config{Factory::IndexEnum::RIndexSampledWithTrustedMarks, 8}},
+      {"R-Index-WS-TM/16", Factory::Config{Factory::IndexEnum::RIndexSampledWithTrustedMarks, 16}},
+      {"R-Index-WS-TM/32", Factory::Config{Factory::IndexEnum::RIndexSampledWithTrustedMarks, 32}},
+      {"R-Index-WS-TM/64", Factory::Config{Factory::IndexEnum::RIndexSampledWithTrustedMarks, 64}},
   };
 
   std::string print_bm_prefix = "Print-";
