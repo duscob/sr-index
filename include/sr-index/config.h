d526af98ce1fabe9334a3dca82160145072c1443
Dustin Cobas <dustin.cobas@gmail.com>
2023-06-24 19:27:06 -0400

    Add SA construction algorithm to Config

--- include/sr-index/config.h
+++ include/sr-index/config.h
@@ -8,16 +8,27 @@
 #include <filesystem>
 
 #include <sdsl/config.hpp>
+#include <sdsl/construct_config.hpp>
 
 namespace sri {
 
+enum SAAlgo {
+  SDSL_LIBDIVSUFSORT,
+  SDSL_SE_SAIS,
+  BIG_BWT
+};
+
 struct Config : public sdsl::cache_config {
   std::filesystem::path data_path;
+  SAAlgo sa_algo = SDSL_LIBDIVSUFSORT;
 
   Config() = default;
 
-  Config(const std::filesystem::path &t_data_path, const std::filesystem::path &t_output_dir)
+  Config(const std::filesystem::path &t_data_path,
+         const std::filesystem::path &t_output_dir,
+         SAAlgo t_sa_algo = SDSL_LIBDIVSUFSORT)
       : data_path(t_data_path),
+        sa_algo(t_sa_algo),
         cache_config(false, t_output_dir, t_data_path.filename()) {
   }
 };
