b591f697f3838e3fcd60d6cb33db2af308df7fcf
Dustin Cobas <dustin.cobas@gmail.com>
2023-08-11 02:03:59 -0400

    Update count benchmarks to support Base64 encoded patterns

--- benchmark/sr-csa/bm_count_csa.cpp
+++ benchmark/sr-csa/bm_count_csa.cpp
@@ -10,6 +10,7 @@
 
 #include <sdsl/config.hpp>
 
+#include "../bm_locate.h"
 #include "factory.h"
 
 DEFINE_string(patterns, "", "Patterns file. (MANDATORY)");
@@ -17,45 +18,18 @@ DEFINE_string(data_dir, "./", "Data directory.");
 DEFINE_string(data_name, "data", "Data file basename.");
 DEFINE_bool(print_result, false, "Execute benchmark that print results per index.");
 
-void SetupDefaultCounters(benchmark::State &t_state) {
-  t_state.counters["Collection_Size(bytes)"] = 0;
-  t_state.counters["Size(bytes)"] = 0;
-  t_state.counters["Bits_x_Symbol"] = 0;
-  t_state.counters["Patterns"] = 0;
-  t_state.counters["Time_x_Pattern"] = 0;
-  t_state.counters["Occurrences"] = 0;
-  t_state.counters["Time_x_Occurrence"] = 0;
-}
-
-// Benchmark Warm-up
-static void BM_WarmUp(benchmark::State &_state) {
-  for (auto _ : _state) {
-    std::vector<int> empty_vector(1000000, 0);
-  }
-
-  SetupDefaultCounters(_state);
-}
-BENCHMARK(BM_WarmUp);
-
 auto BM_QueryCount = [](benchmark::State &t_state, const auto &t_idx, const auto &t_patterns, auto t_seq_size) {
   std::size_t total_occs = 0;
 
   for (auto _ : t_state) {
     total_occs = 0;
     for (const auto &pattern : t_patterns) {
-      auto range = t_idx.idx->Count(pattern);
+      auto range = t_idx.idx->Count(pattern.decoded);
       total_occs += range.second - range.first;
     }
   }
 
-  SetupDefaultCounters(t_state);
-  t_state.counters["Collection_Size(bytes)"] = t_seq_size;
-  t_state.counters["Size(bytes)"] = t_idx.size;
-  t_state.counters["Bits_x_Symbol"] = t_idx.size * 8.0 / t_seq_size;
-  t_state.counters["Patterns"] = t_patterns.size();
-  t_state.counters["Time_x_Pattern"] = benchmark::Counter(
-      t_patterns.size(), benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
-  t_state.counters["Occurrences"] = total_occs;
+  UpdateCounter(t_state, t_seq_size, t_idx.size, t_patterns.size(), total_occs);
 };
 
 auto BM_PrintQueryCount = [](
@@ -71,21 +45,14 @@ auto BM_PrintQueryCount = [](
     out << "pattern,count,range_start,range_end" << std::endl;
     total_occs = 0;
     for (const auto &pattern : t_patterns) {
-      auto range = t_idx.idx->Count(pattern);
+      auto range = t_idx.idx->Count(pattern.decoded);
       auto count = range.second - range.first;
       total_occs += count;
-      out << "\"" << pattern << "\"," << count << "," << range.first << "," << range.second << std::endl;
+      out << "\"" << pattern.encoded << "\"," << count << "," << range.first << "," << range.second << std::endl;
     }
   }
 
-  SetupDefaultCounters(t_state);
-  t_state.counters["Collection_Size(bytes)"] = t_seq_size;
-  t_state.counters["Size(bytes)"] = t_idx.size;
-  t_state.counters["Bits_x_Symbol"] = t_idx.size * 8.0 / t_seq_size;
-  t_state.counters["Patterns"] = t_patterns.size();
-  t_state.counters["Time_x_Pattern"] = benchmark::Counter(
-      t_patterns.size(), benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
-  t_state.counters["Occurrences"] = total_occs;
+  UpdateCounter(t_state, t_seq_size, t_idx.size, t_patterns.size(), total_occs);
 };
 
 int main(int argc, char *argv[]) {
@@ -98,23 +65,7 @@ int main(int argc, char *argv[]) {
   }
 
   // Query patterns
-  std::vector<std::string> patterns;
-  {
-    std::ifstream pattern_file(FLAGS_patterns.c_str(), std::ios_base::binary);
-    if (!pattern_file) {
-      std::cerr << "ERROR: Failed to open patterns file! (" << FLAGS_patterns << ")" << std::endl;
-      return 3;
-    }
-
-    std::string buf;
-    while (std::getline(pattern_file, buf)) {
-      if (buf.empty())
-        continue;
-
-      patterns.emplace_back(buf);
-    }
-    pattern_file.close();
-  }
+  auto patterns = ReadPatterns(FLAGS_patterns);
 
   // Indexes
   sdsl::cache_config config(true, FLAGS_data_dir, FLAGS_data_name);
@@ -134,7 +85,7 @@ int main(int argc, char *argv[]) {
     if (FLAGS_print_result) {
       auto print_bm_name = print_bm_prefix + idx_config.first;
       benchmark::RegisterBenchmark(
-          print_bm_name.c_str(), BM_PrintQueryCount, idx_config.first, index, patterns, factory.sizeSequence());
+          print_bm_name, BM_PrintQueryCount, idx_config.first, index, patterns, factory.sizeSequence());
     }
   }
 
