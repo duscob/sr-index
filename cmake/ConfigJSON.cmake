7487d25054028c501415b3f5012d33612b1426b3
Dustin Cobas <dustin.cobas@gmail.com>
2023-11-27 16:32:06 -0500

    Update cmake module for JSON

--- cmake/ConfigJSON.cmake
+++ cmake/ConfigJSON.cmake
@@ -5,6 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
         ${ExternalProjectName}
         URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
+        DOWNLOAD_EXTRACT_TIMESTAMP true
 )
 
 FetchContent_MakeAvailable(${ExternalProjectName})
