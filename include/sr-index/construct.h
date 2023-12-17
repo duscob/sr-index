90c383541e146aa5000bf52812a44a66be7a7090
Dustin Cobas <dustin.cobas@gmail.com>
2023-12-16 19:10:09 -0500

    Refactor utility functions

--- include/sr-index/construct.h
+++ include/sr-index/construct.h
@@ -42,19 +42,18 @@ void constructIndexBaseItems(const std::string &t_data_path, sri::Config &t_conf
   }
 }
 
-auto KeySortedByAlphabet(const std::string& t_key) {
+inline auto KeySortedByAlphabet(const std::string& t_key) {
   return t_key + "_sorted_alphabet";
 }
 
 template<uint8_t t_width>
-auto sizeIntVector(sdsl::cache_config& t_config, const std::string& t_key) {
+auto sizeIntVector(const sdsl::cache_config& t_config, const std::string& t_key) {
   sdsl::int_vector_buffer<t_width> buffer(sdsl::cache_file_name(t_key, t_config));
   return buffer.size();
 }
 
 inline auto getExtremes(const sdsl::cache_config& t_config, const std::string& t_key) {
   sdsl::int_vector_buffer<> buffer(cache_file_name(t_key, t_config));
-
   return std::array<std::size_t, 2>{buffer[0], buffer[buffer.size() - 1]};
 }
 
