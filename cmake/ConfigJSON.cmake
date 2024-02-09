05ded8d9d483891d610d811023e37d275e18aa1d
Dustin Cobas <dustin.cobas@gmail.com>
2024-02-09 15:05:35 -0500

    Refactor CMake dependencies

--- cmake/ConfigJSON.cmake
+++ cmake/ConfigJSON.cmake
@@ -4,10 +4,13 @@ include(FetchContent)
 
 FetchContent_Declare(
         ${ExternalProjectName}
-        URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
         DOWNLOAD_EXTRACT_TIMESTAMP true
+        URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
 )
 
-FetchContent_MakeAvailable(${ExternalProjectName})
+#FetchContent_MakeAvailable(${ExternalProjectName})
+FetchContent_GetProperties(${ExternalProjectName})
+FetchContent_Populate(${ExternalProjectName})
+add_subdirectory("${${ExternalProjectName}_SOURCE_DIR}" "${${ExternalProjectName}_BINARY_DIR}")
 
-include_directories("${${ExternalProjectName}_SOURCE_DIR}")
+include_directories(${${ExternalProjectName}_SOURCE_DIR})
