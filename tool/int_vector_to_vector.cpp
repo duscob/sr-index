58432e98d4599f2c63ce9926728824c93fdf001a
Dustin Cobas <dustin.cobas@gmail.com>
2024-02-11 13:42:50 -0500

    Add tool to serialize from int_vector to vector

---
+++ tool/int_vector_to_vector.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Dustin Cobas <dustin.cobas@gmail.com> on 2/11/2024.
+//
+
+#include <filesystem>
+#include <fstream>
+
+#include <gflags/gflags.h>
+
+#include <sdsl/int_vector.hpp>
+
+#include "sr-index/config.h"
+
+DEFINE_string(data, "", "Data file. (MANDATORY)");
+DEFINE_string(key, "", "File key. (MANDATORY)");
+
+int main(int argc, char* argv[]) {
+  gflags::AllowCommandLineReparsing();
+  gflags::ParseCommandLineFlags(&argc, &argv, false);
+
+  const std::filesystem::path data_path = FLAGS_data;
+  sri::Config config(data_path, data_path.parent_path(), sri::SDSL_LIBDIVSUFSORT);
+
+  sdsl::int_vector<> ivec;
+  sdsl::load_from_cache(ivec, FLAGS_key, config, true);
+
+  std::ofstream fs(FLAGS_key + "_vec.bin", std::ios::out | std::ios::binary);
+  for (sdsl::int_vector<>::value_type item : ivec) {
+    fs.write(reinterpret_cast<const char *>(&item), sizeof(sdsl::int_vector<>::value_type));
+    // fs.write(&item, sizeof(sdsl::int_vector<>::value_type));
+  }
+  fs.close();
+
+  return 0;
+}
