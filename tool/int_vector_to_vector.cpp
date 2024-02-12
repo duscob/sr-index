5dfe90124558c348736a6c381890def774cf052a
Dustin Cobas <dustin.cobas@gmail.com>
2024-02-12 11:30:13 -0500

    Update int_vector_to_vector tool

--- tool/int_vector_to_vector.cpp
+++ tool/int_vector_to_vector.cpp
@@ -13,6 +13,7 @@
 
 DEFINE_string(data, "", "Data file. (MANDATORY)");
 DEFINE_string(key, "", "File key. (MANDATORY)");
+DEFINE_bool(hash_type, false, "Add hash type to file key.");
 
 int main(int argc, char* argv[]) {
   gflags::AllowCommandLineReparsing();
@@ -22,9 +23,9 @@ int main(int argc, char* argv[]) {
   sri::Config config(data_path, data_path.parent_path(), sri::SDSL_LIBDIVSUFSORT);
 
   sdsl::int_vector<> ivec;
-  sdsl::load_from_cache(ivec, FLAGS_key, config, true);
+  sdsl::load_from_cache(ivec, FLAGS_key, config, FLAGS_hash_type);
 
-  std::ofstream fs(FLAGS_key + "_vec.bin", std::ios::out | std::ios::binary);
+  std::ofstream fs(sdsl::cache_file_name(FLAGS_key, config) + ".vec.bin", std::ios::out | std::ios::binary);
   for (sdsl::int_vector<>::value_type item : ivec) {
     fs.write(reinterpret_cast<const char *>(&item), sizeof(sdsl::int_vector<>::value_type));
     // fs.write(&item, sizeof(sdsl::int_vector<>::value_type));
