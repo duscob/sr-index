ea366c86f4c2b8e5b3c1f40bd3fc7c6f0217ee01
Dustin Cobas <dustin.cobas@gmail.com>
2025-10-11 15:19:54 -0400

    Update nlohmann/json to v3.12.0 and refine FetchContent usage

--- cmake/ConfigJSON.cmake
+++ cmake/ConfigJSON.cmake
@@ -5,12 +5,12 @@ include(FetchContent)
 FetchContent_Declare(
         ${ExternalProjectName}
         DOWNLOAD_EXTRACT_TIMESTAMP true
-        URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz
+        URL https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz
 )
 
-#FetchContent_MakeAvailable(${ExternalProjectName})
-FetchContent_GetProperties(${ExternalProjectName})
-FetchContent_Populate(${ExternalProjectName})
-add_subdirectory("${${ExternalProjectName}_SOURCE_DIR}" "${${ExternalProjectName}_BINARY_DIR}")
-
-include_directories(${${ExternalProjectName}_SOURCE_DIR})
+FetchContent_MakeAvailable(${ExternalProjectName})
+#FetchContent_GetProperties(${ExternalProjectName})
+#FetchContent_Populate(${ExternalProjectName})
+#add_subdirectory("${${ExternalProjectName}_SOURCE_DIR}" "${${ExternalProjectName}_BINARY_DIR}")
+#
+#include_directories(${${ExternalProjectName}_SOURCE_DIR})
