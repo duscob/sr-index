fe896c8b4214c8468e6eae46e65ecd956a13aeef
Dustin Cobas <dustin.cobas@gmail.com>
2025-12-27 16:21:44 -0500

    Add commented example for loading index from separate components in `load_bytes.cpp`

--- tool/load_bytes.cpp
+++ tool/load_bytes.cpp
@@ -8,6 +8,15 @@ int main(int argc, char* argv[]) {
   std::string index_file = "/path/to/index/file";
   sdsl::load_from_file(index, index_file);
 
+  // Loading the full index from individual component files
+  // If you did not store the full index in a single file,
+  // you can use the separated components stored in different files uncommenting the following lines.
+  // std::string data_path = "/path/to/data/file";  // only the data filename is required
+  // auto output_path = std::filesystem::current_path();
+  // sri::Config config(data_path, output_path, sri::SAAlgo::SDSL_SE_SAIS);
+  // index.load(config);
+
+
   // Locating occurrences of the given pattern
   std::vector<std::size_t> result = index.Locate("pattern");
 
